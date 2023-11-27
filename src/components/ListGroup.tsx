81e1c2a80cfbca95a3305b45bd9c6973ded6539b
MAK <97058316+mak360@users.noreply.github.com>
2023-11-27 18:10:05 +0600

    Use props

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 
-const ListGroup = () => {
-  const items = ["Tokyo", "Dhaka", "New York"];
+interface Props {
+  items: string[];
+  heading: string;
+}
+
+const ListGroup = ({ items, heading }: Props) => {
   const [selectedItem, setSelectedItem] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No item found!</p>}
       <ul className="list-group">
         {items.map((item, index) => (
