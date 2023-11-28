b8282fce29f438abd1da9963c970db9e5df7e0ff
MAK <97058316+mak360@users.noreply.github.com>
2023-11-28 17:33:01 +0600

    Add Button & Dismissable Alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import Button from "./components/Button";
+import Alert from "./components/Alert.";
 
 const App = () => {
+  const [alertVisibility, setAlertVisibility] = useState(false);
+
   return (
     <div>
+      {alertVisibility && (
+        <Alert onClose={() => setAlertVisibility(false)}>Hellooo</Alert>
+      )}
+
       <Button
         color="danger"
         onClick={() => {
-          console.log("Yo");
+          setAlertVisibility(true);
         }}
       >
         Hello World
