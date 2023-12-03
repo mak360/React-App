34c428a347ea42fcc898154eaa55f0f2008af22b
MAK <97058316+mak360@users.noreply.github.com>
2023-12-03 16:23:18 +0600

    Using react hook form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,30 +1,33 @@
-import { FormEvent, useRef } from "react";
+import { useForm } from "react-hook-form";
 
 const Form = () => {
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
-  const person = { name: "", age: 0 };
+  const { register, handleSubmit } = useForm();
 
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    if (nameRef.current !== null) person.name = nameRef.current.value;
-    if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
-    console.log(person);
-  };
+  const onSubmit = (data: FieldValues) => console.log(data);
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input ref={nameRef} type="text" className="form-control" id="name" />
+        <input
+          {...register("name")}
+          type="text"
+          className="form-control"
+          id="name"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input ref={ageRef} type="number" className="form-control" id="age" />
+        <input
+          {...register("age")}
+          type="number"
+          className="form-control"
+          id="age"
+        />
       </div>
       <button type="submit" className="btn btn-primary">
         Submit
