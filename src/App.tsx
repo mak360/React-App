34f760053b695609c49f9073062dcced9317b3cc
MAK <97058316+mak360@users.noreply.github.com>
2023-12-03 13:16:04 +0600

    Form submit with useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,7 @@
-import { useState } from "react";
-import Button from "./components/Button";
-import Alert from "./components/Alert.";
+import Form from "./components/Form";
 
 const App = () => {
-  const [alertVisibility, setAlertVisibility] = useState(false);
-
-  return (
-    <div>
-      {alertVisibility && (
-        <Alert onClose={() => setAlertVisibility(false)}>Hellooo</Alert>
-      )}
-
-      <Button
-        color="warning"
-        onClick={() => {
-          setAlertVisibility(true);
-        }}
-      >
-        Hello World
-      </Button>
-    </div>
-  );
+  return <Form />;
 };
 
 export default App;
---
+++ src/components/Form.tsx
@@ -0,0 +1,36 @@
+import { FormEvent, useRef } from "react";
+
+const Form = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const person = { name: "", age: 0 };
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    if (nameRef.current !== null) person.name = nameRef.current.value;
+    if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
+    console.log(person);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input ref={nameRef} type="text" className="form-control" id="name" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input ref={ageRef} type="number" className="form-control" id="age" />
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Form;
